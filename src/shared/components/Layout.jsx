0fdd20cbcbd219ff506685be237bf892824124f2
lfonseca-dev <lucasmarquesfonseca8@gmail.com>
2026-08-29 14:38:20 -0300

    adicionado rotas de admin

--- src/shared/components/Layout.jsx
+++ src/shared/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./NavBar";
+import NavBar from "./NavBarL";
 import { useAuth } from "../../context/AuthContext";
 
 export default function Layout() {
